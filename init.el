9b90ceced428a57750ad3d51d23904398bce51e2
sappy5678 <sappy5678@gmail.com>
2016-07-04 00:16:13 +0800

    add windows control

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;;掃描~/.emacs.d目錄
+(add-to-list 'load-path "~/.emacs.d/rc")
+(require 'rc-windcycle)  ;;a windows控制
+
 ;; package.el 相關設定
 (require 'package)
 (package-initialize)
@@ -18,5 +22,10 @@
 (defun run-buffer-with-nodejs-interpreter ()
   (interactive)
   (save-buffer)
-  (shell-command (format "nodejs %s" (file-name-nondirectory buffer-file-name)))
- )
+  (getenv "SHELL")
+   ;; (shell-command (format "ls" )) 
+  ;;(shell-command (format "nvm use 6.2.2" )) 
+  (shell-command (format "node %s" (file-name-nondirectory buffer-file-name)))  ;;相對路徑
+  ;;(shell-command (format "node %s" (buffer-file-name))) ;;絕對路徑
+
+   )
